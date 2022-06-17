7b24cb78300459bf6d0f2d15bef5f60871d310bf
SeryiBaran <seryibaranminepy@yandex.ru>
2022-06-17 11:23:14 +0300

    Init commit

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,6 @@
+describe('Button with text "Primary" is visible', () => {
+  it('Passes!', () => {
+    cy.visit('http://localhost:4173/cypress-test/');
+    cy.get('button.btn.btn-primary').should('be.visible').contains('Primary');
+  });
+});
